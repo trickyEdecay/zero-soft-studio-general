f50cd8cedcc6048d9416efaf8636c340e4839646
Track <659956865@qq.com>
2019-04-26 20:35:39 +0800

    添加 小怪物游戏随机伤害和30%产生暴击效果（0-10点伤害）

--- monster-js/Track/js.js
+++ monster-js/Track/js.js
@@ -1,10 +1,19 @@
 var hp=100;
 var monsterHpEle=document.getElementById("monster-hp");
+Math.random();
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 function attack() {
-    hp-=10;
+    if(random(0,100)>69){
+        hp-=(random(0,10));
+    }
+    hp-=(random(5,10));
     if(hp<0){
         hp=0;
-        alert("The monster is dead")
+        alert("The monster is dead");
     }
     monsterHpEle.style.width=hp+"%";
+
 }
+
