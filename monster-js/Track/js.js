0bcaccc6b14fc9a383c70f33769df703b8aa11c3
Track <659956865@qq.com>
2019-04-26 21:50:42 +0800

    添加 怪物攻击英雄效果

--- monster-js/Track/js.js
+++ monster-js/Track/js.js
@@ -1,20 +1,34 @@
-var hp=100;
+var monsterhp=100;
+var herohp=100;
 var monsterHpEle=document.getElementById("monster-hp");
+var heroHpEle=document.getElementById("hero-hp");
+
+
 Math.random();
 function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function attack() {
-    if(random(0,100)>69){
-        hp-=(random(0,10));
-        return;
+function heroattack() {
+    if(random(0,100)>50){
+        monsterhp-=(random(0,10));
     }
-    hp-=(random(5,10));
-    if(hp<0){
-        hp=0;
+    monsterhp-=(random(5,10));
+    if(monsterhp<0){
+        monsterhp=0;
         alert("The monster is dead");
     }
-    monsterHpEle.style.width=hp+"%";
-
+    monsterHpEle.style.width=monsterhp+"%";
 }
+function monsterattack(){
+    if(random(0,100)>90){
+        herohp-=(random(0,10));
+    }
+    herohp-=(random(5,10));
+    if(herohp<0){
+        herohp=0;
+        alert("you are dead");
+    }
+    heroHpEle.style.width=herohp+"%";
+}
+
 
