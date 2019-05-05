59bac28af726547cf283cb49df12252aeca1ddf4
Track <659956865@qq.com>
2019-05-05 20:30:11 +0800

    添加 本地存储和优化 其他功能

--- navigation-js/Track/main.js
+++ navigation-js/Track/main.js
@@ -27,12 +27,9 @@ var addEle=document.getElementById("add");
 function eject(){
     document.getElementById("shade").classList.add('appear')
     document.getElementById("collection").classList.add('appear')
-
 }
 /*给+号添加一个点击事件监听器*/
 addEle.addEventListener('click',eject);
-
-
 
 
 /*获得取消按钮元素*/
@@ -45,48 +42,67 @@ function disappear(){
 /*给取消按钮添加一个点击监听器*/
 disappearBtnEle.addEventListener('click',disappear);
 
-/*产生相应*/
+
+
+var shadeEle=document.getElementById("shade");
+
+/*点击遮罩层会取消该窗口*/
+function Return() {
+    document.getElementById("shade").classList.remove('appear');
+    document.getElementById("collection").classList.remove('appear');
+
+}
+/*给遮罩层添加一个点击事件*/
+shadeEle.addEventListener('click',Return);
+/*给第三个输入框 输入完后按回车就会直接产生一个导航栏*/
+document.getElementById("add-link").onkeydown = function(){
+    if(event.keyCode == 13){
+        construct();
+        Return();
+    }
+}
+
+
+
+/*产生相应导航栏*/
 function construct() {
-    var layoutMainEle = document.getElementById("abc").parentElement;
+    var layoutMainEle = document.getElementById("first").parentElement;
     var newaEle = document.createElement("a");
     layoutMainEle.append(newaEle);
 
 
     var newContainerEle = document.createElement("div");
     newaEle.append(newContainerEle);
-    newContainerEle.setAttribute("class", "container");
+    newContainerEle.classList.add('container');
 
-    var newContainerLeftEle = document.createElement("div")
+    var newContainerLeftEle = document.createElement("div");
     newContainerEle.append(newContainerLeftEle);
-    newContainerLeftEle.setAttribute("class","container-left");
+    newContainerLeftEle.classList.add('container-left');
 
-    var newContainerRightEle = document.createElement("div")
+    var newContainerRightEle = document.createElement("div");
     newContainerEle.append(newContainerRightEle);
-    newContainerRightEle.setAttribute("class","container-right");
+    newContainerRightEle.classList.add('container-right');
 
     /*container-left*/
     var circularEle = document.createElement("div");
     newContainerLeftEle.append(circularEle);
-    circularEle.setAttribute("class", "yuan");
+    circularEle.classList.add('yuan');
 
     var pictureEle = document.createElement("div");
     circularEle.append(pictureEle);
-    pictureEle.setAttribute("class", "picture");
+    pictureEle.classList.add('picture');
 
     var imgEle = document.createElement("img");
     pictureEle.append(imgEle);
-    imgEle.setAttribute("src", "aixin.png");
+    imgEle.setAttribute("src","aixin.png");
 
-    /*container-right*/
     var titleEle = document.createElement("div");
     newContainerRightEle.append(titleEle);
-    titleEle.setAttribute("class", "title")
+    titleEle.classList.add('title');
 
     var describeEle = document.createElement("div");
     newContainerRightEle.append(describeEle);
-    describeEle.setAttribute("class","describe");
-
-
+    describeEle.classList.add('describe');
 
 
 
@@ -101,24 +117,89 @@ function construct() {
     d=inpuLinkEle.value;
 
     titleEle.innerHTML=b;
-    describeEle.innerHTML=c
+    describeEle.innerHTML=c;
     newaEle.setAttribute("href","https://"+d);
 
-    /*var addTitleEle=titleEle.document.createTextNode("b");
-    var addescribeEle=describeEle.document.createTextNode("c");
-
-    inputTitleEle.appendChild(addTitleEle);
-    inputDescribeEle.appendChild(addescribeEle);*/
 
 
+    var number;
+    number=i+1;
 
+    localStorage.setItem("number",number);
+    localStorage.setItem("title"+i,b);
+    localStorage.setItem("describe"+i,c);
+    localStorage.setItem("link"+i,d);
+    i++;
 }
 
+var i=0;
+
+
+
+
 var trueBtnEle=document.getElementById("true-btn");
-
-
-
-
+/*给添加按钮添加一个事件监听器 按下时候遮罩层会取消*/
 trueBtnEle.addEventListener('click',construct);
 trueBtnEle.addEventListener('click',disappear);
+
+
+/*本地存储
+* 如果没有设置就不会出来
+* 设置后F5将会存储中取出构成样式*/
+function read(){
+    if(localStorage.getItem("title"+i)!=null){
+        for (var t=0;t<localStorage.getItem("number");t++){
+            var layoutMainEle = document.getElementById("first").parentElement;
+            var newaEle = document.createElement("a");
+            layoutMainEle.append(newaEle);
+
+
+            var newContainerEle = document.createElement("div");
+            newaEle.append(newContainerEle);
+            newContainerEle.classList.add('container');
+
+            var newContainerLeftEle = document.createElement("div");
+            newContainerEle.append(newContainerLeftEle);
+            newContainerLeftEle.classList.add('container-left');
+
+            var newContainerRightEle = document.createElement("div");
+            newContainerEle.append(newContainerRightEle);
+            newContainerRightEle.classList.add('container-right');
+
+            /*container-left*/
+            var circularEle = document.createElement("div");
+            newContainerLeftEle.append(circularEle);
+            circularEle.classList.add('yuan');
+
+            var pictureEle = document.createElement("div");
+            circularEle.append(pictureEle);
+            pictureEle.classList.add('picture');
+
+            var imgEle = document.createElement("img");
+            pictureEle.append(imgEle);
+            imgEle.setAttribute("src","aixin.png");
+
+            var titleEle = document.createElement("div");
+            newContainerRightEle.append(titleEle);
+            titleEle.classList.add('title');
+            titleEle.innerHTML=localStorage.getItem('title'+t);
+
+            var describeEle = document.createElement("div");
+            newContainerRightEle.append(describeEle);
+            describeEle.classList.add('describe');
+            describeEle.innerHTML=localStorage.getItem('describe'+t);
+
+
+
+            newaEle.setAttribute("href","https://"+localStorage.getItem('link'+t));
+        }
+
+    }
+}
+read();
+
+
+
+
+
 
