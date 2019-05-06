9a0f46fc6237c19a52bd87781b6a3132089439e7
ddloo <1249129864@qq.com>
2019-05-07 00:05:07 +0800

    添加ddloo导航-js的页面

---
+++ navigation-js/ddloo/js/add.js
@@ -0,0 +1,9 @@
+function add(){
+    var add = document.getElementById("add");
+    add.style.transition = "width .2s, height .2s";
+    add.style.height = "100%";
+}
+function hide(){
+    var add = document.getElementById("add");
+    add.style.height = "0";
+}
