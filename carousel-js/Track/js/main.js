904a229075f207bc19c62b2e380a4931b077d5b1
Track <659956865@qq.com>
2019-06-02 10:58:00 +0800

    优化 less 完善js

--- carousel-js/Track/js/main.js
+++ carousel-js/Track/js/main.js
@@ -4,68 +4,60 @@ var slideShow = $("#inner-container")
 var picWidth=400;
 /* 当前位置 */
 var position=0
-/*  */
+/* 下面三个小店 */
 var indicatorBtn = $(".indicator")
-
+/* 左边按钮 */
 var LeftBtn=$("#left-btn")
-
+/* 右边按钮 */
 var RightBtn=$("#right-btn")
 /* 计时器 */
 var timer=null;
-
+/* 页面序号 */
+var iNow=1;
+/* 三个小店亮灭并跳转到相应页面 */
 indicatorBtn.on("click",function(){
-    $(this).addClass("spot1").siblings().removeClass("spot1");
+    $(this).addClass("active").siblings().removeClass("active");
     var index=$(this).index();
-    console.log(iNow);
-    switch(index){
+    iNow=index;
+    switch(iNow){
         case 0:
-        $("#inner-container").animate({left:0});
+            position=0;
+            slideShow.css('transform','translateX('+position+'px)');
         break;
         case 1:
-        $("#inner-container").animate({left:-(picWidth*1)});
+            position=-picWidth;
+            slideShow.css('transform','translateX('+position+'px)')
         break;
         case 2:
-        $("#inner-container").animate({left:-(picWidth*2)});
+            position=-picWidth*2;
+            slideShow.css('transform','translateX('+position+'px)') 
         break;
     }
 })
-
-
-/* 页面序号 */
-var iNow=0;
-indicatorBtn.eq(iNow).trigger("click");
-timer=setInterval(function(){
-    if(iNow>3){
-        iNow=0;
-    }
-    indicatorBtn.eq(iNow).trigger("click");
-
-    iNow++;
-},3000)
-
-
-
-
-
-
-
+/* 计时器 */
+indicatorBtn.eq(0).addClass("active").siblings().removeClass("active");
+timer=setInterval(jump,3000)
+function jump(){
+        if(iNow>2){
+            iNow=0;
+        }
+        indicatorBtn.eq(iNow).trigger("click");
+        iNow++;
+}
 /* 左边按钮绑定事件 */
 LeftBtn.on("click",function(){
     switch(iNow){
+        case 0:
+            iNow=2;
+            indicatorBtn.eq(iNow).trigger("click")
+        break;
         case 1:
-        position=-(picWidth*2);
-        $("#inner-container").animate({left:position});
-        iNow=3;
+            iNow=0;
+            indicatorBtn.eq(iNow).trigger("click");   
         break;
         case 2:
-        position=0;
-        $("#inner-container").animate({left:position});
-        iNow=1;
-        break;
-        case 3:
-        position=-(picWidth);
-        $("#inner-container").animate({left:position});
-        iNow=2;
+            iNow=1;
+            indicatorBtn.eq(iNow).trigger("click");
         break;
     }
 })
@@ -73,20 +65,17 @@ LeftBtn.on("click",function(){
 /* 给右边绑定事件 */
 RightBtn.on("click",function(){
     switch(iNow){
+        case 0:
+            iNow=1;
+            indicatorBtn.eq(iNow).trigger("click")
+        break;
         case 1:
-        position=-picWidth;
-        $("#inner-container").animate({left:position});
-        iNow=2;
+            iNow=2;
+            indicatorBtn.eq(iNow).trigger("click")
         break;
         case 2:
-        position=-(picWidth*2);
-        $("#inner-container").animate({left:position});
-        iNow=3
-        break;
-        case 3:
-        position=0;
-        $("#inner-container").animate({left:position});
-        iNow=1
+            iNow=0;
+            indicatorBtn.eq(iNow).trigger("click")  
         break;
     }
 })
